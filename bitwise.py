4af80f66b040b0ca0f51941d09070755b522e70d
Antonio Kezele PC <akezele35@gmail.com>
2022-08-06 14:22:26 +0200

    Improved bitwise operations

--- bitwise.py
+++ bitwise.py
@@ -5,8 +5,10 @@ def bitor(image,image2,shape,shape2):
 
     for i in range(shape[0]):
         for i1 in range(shape[1]):
-            if any(image[i,i1])!=0 or any(image2[i,i1])!=0: 
+            if any(image[i,i1])!=0:
                 new_img[i,i1] = image[i,i1]
+            if any(image2[i,i1])!=0 and any(image[i,i1])==0: 
+                new_img[i,i1] = image2[i,i1]
             
 
     return new_img
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import filter
 import numpy as np
 
 img_folder = "images\\"
-img_file = "crops.jpg"
+img_file = "letters - Copy (2).jpg"
 pattern = "images\\face_pattern.jpg"
 img_save = "worked_on.jpg"
 img_path = img_folder+img_file
@@ -13,9 +13,11 @@ img_path_save = img_folder+img_save
 # pattern = cv2.imread(pattern)
 # pattern = cv2.cvtColor(pattern,cv2.COLOR_BGR2GRAY)
 img = cv2.imread(img_path)
-mask = cv2.imread("images\\mask.jpg")
+mask = cv2.imread("images\\letters - Copy.jpg")
+
+print(img)
 print(mask.shape,img.shape)
-img = filter.bitwise_and(img,mask)
+img = filter.bitwise_and(mask,img)
 # cv2.imshow("img",img)
 cv2.imwrite(img_path_save,img)
 
